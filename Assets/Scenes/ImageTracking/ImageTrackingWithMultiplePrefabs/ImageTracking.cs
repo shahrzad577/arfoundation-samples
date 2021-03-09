01619526ae8807838b4455abb7ad8d4843d5c460
zohreh-sh <zohreh-sh@tamu.edu>
2021-03-08 21:32:58 -0600

    BRICKXAR ImageTracking
    1.Image Tracking C# code is written based on youtube tutorial.
    2. two images are added to XRReference as tracking images for two prefabs : 1) Gizmo set 2)ArcDeTriomphe

---
+++ Assets/Scenes/ImageTracking/ImageTrackingWithMultiplePrefabs/ImageTracking.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ImageTracking : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] placablePrefabs;
+
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+    private ARTrackedImageManager trackedImageManager;
+
+
+    private void Awake()
+    {
+        trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+
+        foreach (GameObject prefab in placablePrefabs)
+        {
+            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            newPrefab.name = prefab.name;
+            spawnedPrefabs.Add(prefab.name, newPrefab);
+        }
+    }
+
+    void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += ImageChanged;
+    }
+
+    void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= ImageChanged;
+    }
+
+    void ImageChanged(ARTrackedImagesChangedEventArgs eventARgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventARgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventARgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventARgs.removed)
+        {
+            spawnedPrefabs[trackedImage.name].SetActive(false);
+        }
+
+    }
+
+    void UpdateImage(ARTrackedImage trackedImage)
+    {
+        string name = trackedImage.referenceImage.name;
+        Vector3 position = trackedImage.transform.position;
+
+        GameObject prefab = spawnedPrefabs[name];
+        prefab.transform.position = position;
+        prefab.SetActive(true);
+
+        foreach (GameObject go in spawnedPrefabs.Values)
+        {
+            if (go.name != name)
+                go.SetActive(false);
+        }
+
+    }
+}
+
+
